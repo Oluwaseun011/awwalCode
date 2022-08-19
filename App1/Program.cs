c48d349cf3359fdfb72b9845f2a4d3b92147ba16
unknown <oluwaseunidowu011@gmail.com>
2022-08-19 11:51:36 +0100

    now

---
+++ App1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] b = { 3, 4, 5, 1, 2 };
+            Console.WriteLine(Circuit(a,b));
+        }
+
+        static int Circuit(int[] a, int[] b)
+        {
+            int startPoint = -1;
+            for(int i = 0; i < a.Length; i++)
+            {
+                int sum = 0;
+                for(int j = i; j <= a.Length + i ; j++)
+                {
+                    if(j ==  a.Length-1)
+                    {
+                        int z = j - a.Length-1;
+                        sum += (a[j] - b[j] + a[z]);
+                    }
+                    else if (j >= a.Length - 1)
+                    {
+                        int z = j - a.Length - 1;
+                        sum += (a[z] - b[z] + a[z + 1]);
+                    }
+                    else
+                    {
+                        sum += (a[j] - b[j] + a[j + 1]);
+                        
+                    }
+
+                    if (sum < 0) break;
+
+
+
+                }
+                startPoint = sum;
+            }
+
+            return startPoint;
+        }
+    }
+}
